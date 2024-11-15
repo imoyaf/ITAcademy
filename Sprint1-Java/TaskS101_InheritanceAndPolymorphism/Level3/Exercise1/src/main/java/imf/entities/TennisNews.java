e3c3e6b5aec65bdd0a01dc04ba46aa099128ec80
Ignasi Moya <imoyaf@gmail.com>
2024-11-15 20:13:19 +0100

    Add Tennis News class with methods for calculating price and rating

---
+++ Sprint1-Java/TaskS101_InheritanceAndPolymorphism/Level3/Exercise1/src/main/java/imf/entities/TennisNews.java
@@ -0,0 +1,45 @@
+package imf.entities;
+
+public class TennisNews extends News {
+    private String competition;
+    private String player;
+
+    public TennisNews(String headline, Journalist journalist, String competition, String player) {
+        super(headline, journalist);
+        this.competition = competition;
+        this.player = player;
+    }
+
+    public String getCompetition() {
+        return competition;
+    }
+
+    public void setCompetition(String competition) {
+        this.competition = competition;
+    }
+
+    public String getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(String player) {
+        this.player = player;
+    }
+
+    public float calculateArticlePrice() {
+        float articlePrice = 150;
+        articlePrice += (this.player.equalsIgnoreCase("Federer") || this.player.equalsIgnoreCase("Nadal")
+                || this.player.equalsIgnoreCase("Djokovic")) ? 100F : 0;
+
+        return articlePrice;
+    }
+
+    public short calculateNewsRating() {
+        short newsRating = 4;
+        newsRating += (this.player.equalsIgnoreCase("Federer")
+                || this.player.equalsIgnoreCase("Nadal")
+                || this.player.equalsIgnoreCase("Djokovic")) ? (short)3 : 0;
+
+        return newsRating;
+    }
+}
