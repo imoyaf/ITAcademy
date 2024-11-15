3ebdc2f5afcf69a5f2aa3683e6b814695bbb7631
Ignasi Moya <imoyaf@gmail.com>
2024-11-15 19:42:55 +0100

    Add Journalist class

---
+++ Sprint1-Java/TaskS101_InheritanceAndPolymorphism/Level3/Exercise1/src/main/java/imf/entities/Journalist.java
@@ -0,0 +1,10 @@
+package imf.entities;
+
+import java.util.List;
+
+public class Journalist {
+    private String name;
+    private final String id;
+    private static int salary;
+    private List<News> news;
+}
