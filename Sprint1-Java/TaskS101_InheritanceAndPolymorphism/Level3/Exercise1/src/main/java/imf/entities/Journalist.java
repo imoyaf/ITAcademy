c63db4762f8fef47a98ac507bfbcf3e71a4ccf37
Ignasi Moya <imoyaf@gmail.com>
2024-11-15 19:49:56 +0100

    Complete Journalist class

--- Sprint1-Java/TaskS101_InheritanceAndPolymorphism/Level3/Exercise1/src/main/java/imf/entities/Journalist.java
+++ Sprint1-Java/TaskS101_InheritanceAndPolymorphism/Level3/Exercise1/src/main/java/imf/entities/Journalist.java
@@ -1,10 +1,45 @@
 package imf.entities;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Journalist {
     private String name;
     private final String id;
-    private static int salary;
+    private static final short SALARY = (short)150;
     private List<News> news;
+
+    public Journalist(String name, String id) {
+        this.name = name;
+        this.id = id;
+        this.news = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public List<News> getNews() {
+        return news;
+    }
+
+    public void setNews(List<News> news) {
+        this.news = news;
+    }
+
+    @Override
+    public String toString() {
+        return "Journalist\n"
+                + "Name: " + this.name + "\n"
+                + "Id: " + this.id + "\n"
+                + "News: \n" + this.news.toString();
+    }
 }
